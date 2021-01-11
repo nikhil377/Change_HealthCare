06fcc249df6d20b5e46aeb1eba27e46ccde413d2
Nikhil Arora <arora.nikhil1996@gmail.com>
2021-01-11 16:57:54 +0530

    id changes

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -6,10 +6,10 @@ export default function Header(props) {
             <img src={home} alt="Home button" width="30" height="40"/>
             <div>Select a tenant:</div>
             <select className="company-dropdown">
-                    <option value="volvo">EIN public org</option>
-                    <option value="saab">ABC</option>
-                    <option value="mercedes">Impetus</option>
-                    <option value="audi">Intellicus</option>
+                    <option value="EIN public org">EIN public org</option>
+                    <option value="ABC">ABC</option>
+                    <option value="Impetus">Impetus</option>
+                    <option value="Intellicus">Intellicus</option>
              </select>
         </div>
        
